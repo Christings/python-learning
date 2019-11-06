bc9f0759e9ca814450aeefd141c36119aad01619
Gladysgong <531404376@qq.com>
2019-11-06 18:51:47 +0800

    douban api

--- djangoApps/users/serializers.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# @contact: voidqueens@hotmail.com
-# @software: PyCharm
-# @time: 2019/11/5 下午7:29
-# @site: www.gongyanli.com
-# @file: serializers.py
